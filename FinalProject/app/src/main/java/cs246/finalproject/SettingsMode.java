16dfef05bbaa8aa7aef75784469dd8616564d785
chives <swainstoncory@gmail.com>
2017-06-15 23:41:53 -0600

    Firebase connection and settings adjustment

--- FinalProject/app/src/main/java/cs246/finalproject/SettingsMode.java
+++ FinalProject/app/src/main/java/cs246/finalproject/SettingsMode.java
@@ -1,9 +1,12 @@
 package cs246.finalproject;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsMode extends AppCompatActivity {
 
@@ -17,11 +20,14 @@ public class SettingsMode extends AppCompatActivity {
         preferences = getSharedPreferences(MainActivity.USER_INFO, 0);
     }
 
-    void onSubmit(){
+    void onSubmit(View view){
         EditText editText = (EditText) findViewById(R.id.editText);
         String username = editText.getText().toString();
         SharedPreferences.Editor editor = preferences.edit();
         editor.putString(MainActivity.USERNAME, username);
         editor.apply();
+        Toast.makeText(this, "Name saved!", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
